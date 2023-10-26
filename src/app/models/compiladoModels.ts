f7ef72f6cf8c0b80b7cbbffbb4121a2de3812908
Marcelo Garita <mgaritaf@outlook.com>
2023-10-26 17:35:15 -0600

    models file

--- src/app/models/compiladoModels.ts
+++ src/app/models/compiladoModels.ts
@@ -1,14 +1,14 @@
 export class CompiladoModels{
     ID_Compilado!: number;
-    Oficina!: string;
-    Area!:string;
-    Codigo_Programa!:number;
-    Codigo_Subpartida!:number;
-    ID_Productos!:number;
-    Periodo_Ejecucion!:string;
-    Prioridad!:number;
-    Tipo_Tramite!:string;
-    Numero_Contrato_Vigente!:string;
-    Requision_Certificacion!:string;
-    Numero_SC_UC_UA!:number;
+    Oficina?: string;
+    Area?:string;
+    Codigo_Programa?:number;
+    Codigo_Subpartida?:number;
+    ID_Productos?:number;
+    Periodo_Ejecucion?:string;
+    Prioridad?:number;
+    Tipo_Tramite?:string;
+    Numero_Contrato_Vigente?:string;
+    Requision_Certificacion?:string;
+    Numero_SC_UC_UA?:string;
 }
