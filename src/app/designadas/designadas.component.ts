63091e4f94ed5b5edf7877fe0b39a77f8928b3ff
Marcelo Garita <mgaritaf@outlook.com>
2023-10-26 17:36:53 -0600

    designar component

--- src/app/designadas/designadas.component.ts
+++ src/app/designadas/designadas.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiconnectService } from '../Services/apiconnect.service';
 
 @Component({
   selector: 'app-designadas',
   templateUrl: './designadas.component.html',
   styleUrls: ['./designadas.component.css']
 })
-export class DesignadasComponent {
+export class DesignadasComponent implements OnInit{
+  p:number=1;
+  datos:any;
+  constructor(private service:ApiconnectService){
 
+  }
+  ngOnInit(): void {
+    this.cargaAsignaciones();
+  }
+  cargaAsignaciones(){
+    this.service.ObtenerAsig().subscribe(result=>{
+      console.log(result)
+      this.datos=result;
+    })
+  }
 }
