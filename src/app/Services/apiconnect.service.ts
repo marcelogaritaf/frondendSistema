ebf5add65102048648ce2f0a1d8e18bbfecffa88
Marcelo Garita <mgaritaf@outlook.com>
2023-10-26 17:34:38 -0600

    servicios api

--- src/app/Services/apiconnect.service.ts
+++ src/app/Services/apiconnect.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { CompiladoModels } from '../models/compiladoModels';
 // export interface dataCompilado{
 //   oficina:string,
@@ -9,17 +9,36 @@ import { CompiladoModels } from '../models/compiladoModels';
   providedIn: 'root'
 })
 export class ApiconnectService {
-  AppUrl="http://localhost:5274/";
+  AppUrl="http://localhost:5213/";
+  private actulizarData = new BehaviorSubject <any>({}as any)
   constructor( private http: HttpClient ) {
   }
   //metodo get 
   ObtenerArticulos(){
     return this.http.get(this.AppUrl+"api/Productos/rutaApi");
   }
-  addData(compiladoData:CompiladoModels): Observable<CompiladoModels> {
-    return this.http.post<CompiladoModels>(this.AppUrl+"api/Compilado/Agregar", compiladoData);
-  }
   ObtenerDatos(): Observable<any> {
     return this.http.get(this.AppUrl+"api/Compilado/Compilado");
+  }
+  ObtenerAsig(): Observable<any>{
+    return this.http.get(this.AppUrl+"api/Asignados/Obtener")
+  }
+  addData(compiladoData:any): Observable<any> {
+    return this.http.post<any>(this.AppUrl+"api/Compilado/Agregar", compiladoData);
+  }
+  addAsig(asignarData:any): Observable<any>{
+    return this.http.post<any>(this.AppUrl+"api/Asignados/Agregar",asignarData);
+  }
+  eliminarData(idProductos:number): Observable<any>{
+    return this.http.delete(this.AppUrl+"api/Compilado/Eliminar/"+idProductos)
+  }
+  actualizar(data: any){
+    this.actulizarData.next(data);
+  }
+  updateData(idProductos:number, data:any):Observable<any>{
+    return this.http.put<any>(this.AppUrl+"api/Compilado/Editar"+idProductos,data);
+  }
+  getData$(): Observable<any>{
+    return this.actulizarData.asObservable();
   }
 }
