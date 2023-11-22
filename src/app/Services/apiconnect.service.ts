8cd35e8b5be2d5dbc3280b6b059f903765fe87a0
Marcelo Garita <mgaritaf@outlook.com>
2023-11-21 19:07:34 -0600

    url updated

--- src/app/Services/apiconnect.service.ts
+++ src/app/Services/apiconnect.service.ts
@@ -9,7 +9,7 @@ import { CompiladoModels } from '../models/compiladoModels';
   providedIn: 'root'
 })
 export class ApiconnectService {
-  AppUrl="http://localhost:5158/";
+  AppUrl="http://localhost:5182/";
   private actulizarData = new BehaviorSubject <any>({}as any)
   constructor( private http: HttpClient ) {
   }
